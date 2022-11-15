6c599f51844fef7fbc03c6a1e7c0d43fe066f72e
Alexei-Safyannikov <safyannikov.ao@yandex.ru>
2022-11-15 20:05:37 +0300

    первая задача

---
+++ Задача 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход принимает 2 числа
+// Выдает Max и Min
+Console.WriteLine("Введите целое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2) 
+{
+    Console.WriteLine($"Max = {number1} Min = {number2}");
+}
+else Console.WriteLine ($"Min = {number1} Max = {number2}");
