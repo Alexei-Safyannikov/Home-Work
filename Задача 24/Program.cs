fe9b79824461b2ee299467dd89475c5cb6fe8d0c
Alexei-Safyannikov <safyannikov.ao@yandex.ru>
2022-11-23 19:27:56 +0300

    ДЗ 3 выполнено

---
+++ Задача 24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая 
+// принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+void Kub(int n)
+{
+    while (index <= number)
+    {
+        Console.WriteLine($"{index,4} {Math.Pow(index, 3),4}");
+        index++;
+    }
+}
+if (number > 0)
+{
+    Kub(number);
+}
+else Console.WriteLine("Введите число больше 0");
