0261265f071e8dc2202b721f0f4a68088bf2c4fe
Alexei-Safyannikov <safyannikov.ao@yandex.ru>
2022-11-28 15:32:59 +0300

    иправил решение в 27 задаче на методы

--- Задача 27/Program.cs
+++ Задача 27/Program.cs
@@ -11,26 +11,18 @@
 
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int a = 0;
-if (number > 1)
+
+int NumberDigit(int num)
 {
-    while (number > 1)
+    int res = 0;
+    int i = 0;
+    while (num != 0)
     {
-        a = number % 10;
-        sum = sum + a;
-        number = number / 10;
+        i = num % 10;
+        res = res + i;
+        num = num / 10;
     }
+    return res < 0 ? res=res*-1 : res;
 }
-else if (number < 1)
-{
-    while (number < -1)
-    {
-        a = number % 10;
-        sum = sum + a;
-        number = number / 10;
-    }
-    sum = sum * -1;
-}
-else Console.WriteLine("Сумма цифр в числе = 0");
+int sum = NumberDigit(number);
 Console.WriteLine($"Сумма цифр в числе = {sum}");
